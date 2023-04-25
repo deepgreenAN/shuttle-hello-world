c4b559b2d64df2e3e895b293e01c5f2d628ca676
deepgreenAN <asami73dgreen63@gmail.com>
2023-04-25 18:44:00 +0900

    add db migration

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use my_crate::hello_world2;
 
 use axum::{routing::get, Router};
-
-// async fn hello_world() -> &'static str {
-//     "Hello, world!"
-// }
+use shuttle_runtime::CustomError;
+use sqlx::PgPool;
 
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
+async fn axum(
+    #[shuttle_shared_db::Postgres(
+        local_uri = "postgres://postgres:{secrets.PASSWORD}@localhost/shuttle_example"
+    )]
+    pool: PgPool,
+) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!("./migrations")
+        .run(&pool)
+        .await
+        .map_err(|_| CustomError::msg("migration error."))?;
+
     let router = Router::new().route("/", get(hello_world2));
 
     Ok(router.into())
