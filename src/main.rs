626cc9c91515440aaea9e7321dcb3c20b5f81f9e
deepgreenAN <asami73dgreen63@gmail.com>
2023-04-25 19:17:02 +0900

    add dir serve.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use my_crate::hello_world2;
 
-use axum::{routing::get, Router};
+use std::path::Path;
+
+use axum::{
+    http::StatusCode,
+    routing::{get, get_service},
+    Router,
+};
 use shuttle_runtime::CustomError;
 use sqlx::PgPool;
+use tower_http::services::ServeDir;
 
 #[shuttle_runtime::main]
 async fn axum(
@@ -16,7 +23,13 @@ async fn axum(
         .await
         .map_err(|_| CustomError::msg("migration error."))?;
 
-    let router = Router::new().route("/", get(hello_world2));
+    let dist_path = Path::new("./public");
+    let serve_dir = ServeDir::new(dist_path);
+
+    let router = Router::new().route("/", get(hello_world2)).nest_service(
+        "/public",
+        get_service(serve_dir).handle_error(|_| async move { StatusCode::INTERNAL_SERVER_ERROR }),
+    );
 
     Ok(router.into())
 }
